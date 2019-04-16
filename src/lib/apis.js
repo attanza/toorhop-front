411efdd1aefc230308f90325691208b3294dd836
attanza <myrbx.747@gmail.com>
2019-04-16 10:23:44 +0700

    reffactor api endpoint to staging

--- src/lib/apis.js
+++ src/lib/apis.js
@@ -1,5 +1,5 @@
-export const API_URL = 'http://localhost:4444/api/dev-v1'
-// export const API_URL = 'https://midtrans.toorhop.com/dev-api/v1'
+// export const API_URL = 'http://localhost:4444/api/dev-v1'
+export const API_URL = 'https://midtrans.toorhop.com/api/dev-v1'
 // export const API_URL = 'https://midtrans.toorhop.com/api/v1'
 export const SNAP_TOKEN_URL = `${API_URL}/snap-token`
 export const NOTIF_HANDLER_URL = `${API_URL}/midtrans-notification-handler`
