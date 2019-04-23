aad1077d254a1ca0da07bde954f25ed9558a0b92
attanza <myrbx.747@gmail.com>
2019-04-23 17:15:32 +0700

    refactor notification handler

--- src/lib/apis.js
+++ src/lib/apis.js
@@ -1,4 +1,4 @@
-export const API_URL = 'https://midtrans.toorhop.com/api/dev-v1'
+export const API_URL = "https://midtrans.toorhop.com/api/dev-v1";
 // export const API_URL = 'https://midtrans.toorhop.com/api/v1'
-export const SNAP_TOKEN_URL = `${API_URL}/snap-token`
-export const NOTIF_HANDLER_URL = `${API_URL}/midtrans-notification-handler`
+export const SNAP_TOKEN_URL = `${API_URL}/snap-token`;
+export const NOTIF_HANDLER_URL = `${API_URL}/midtrans-callback`;
